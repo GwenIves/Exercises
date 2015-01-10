b5ba56269d43b8d692d1ac3538f2c9f7ba37c50a
Gwen Ives <ives.gwen@gmail.com>
2015-01-10 10:58:11 +0100

    Added reverse printing

--- tcpl/1_4_conversion.c
+++ tcpl/1_4_conversion.c
@@ -1,17 +1,45 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define LOWER	-100
 #define UPPER	200
 #define STEP	10
 
-int main () {
+static void print_temperatures (int);
+
+int main (int argc, char ** argv) {
+	bool reverse = false;
+
+	int arg = 0;
+
+	while ((arg = getopt (argc, argv, "r")) != -1) {
+		switch (arg) {
+			case 'r':
+				reverse = true;
+				break;
+			case '?':
+			default:
+				break;
+		}
+	}
+
 	printf ("%10s   %10s\n", "Celsius", "Fahrenheit");
 	printf ("%10s---%10s\n", "-------", "----------");
 
-	for (int celsius = LOWER; celsius <= UPPER; celsius += STEP) {
-		double fahrenheit = (9.0 / 5.0 * celsius) + 32.0;
-		printf ("%10d   %10.1f\n", celsius, fahrenheit);
+	if (reverse) {
+		for (int celsius = UPPER; celsius >= LOWER; celsius -= STEP)
+			print_temperatures (celsius);
+	} else {
+		for (int celsius = LOWER; celsius <= UPPER; celsius += STEP)
+			print_temperatures (celsius);
 	}
 
 	return 0;
+}
+
+static void print_temperatures (int celsius) {
+	double fahrenheit = (9.0 / 5.0 * celsius) + 32.0;
+
+	printf ("%10d   %10.1f\n", celsius, fahrenheit);
 }
