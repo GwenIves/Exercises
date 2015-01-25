42603cb0d3082c2dfb0585b396aafdc6e31cab97
Gwen Ives <ives.gwen@gmail.com>
2015-01-25 11:07:55 +0100

    Added unescaping

--- tcpl/1_10_visible_white_space.c
+++ tcpl/1_10_visible_white_space.c
@@ -1,28 +1,88 @@
 /*
- * Copy stdin to stdout replacing tabs with \t and backspace with \b
+ * Copy stdin to stdout escaping/unescaping special characters
  */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-int main (void) {
+static void escape (int);
+static void unescape (int);
+
+int main (int argc, char ** argv) {
+	bool should_escape = true;
+
+	int arg = 0;
+
+	while ((arg = getopt (argc, argv, "u")) != -1) {
+		switch (arg) {
+			case 'u':
+				should_escape = false;
+				break;
+			case '?':
+			default:
+				break;
+		}
+	}
+
 	int c = 0;
 
-	while ((c = getchar ()) != EOF) {
+	if (should_escape) {
+		while ((c = getchar ()) != EOF)
+			escape (c);
+	} else {
+		while ((c = getchar ()) != EOF)
+			unescape (c);
+	}
+
+	return 0;
+}
+
+static void escape (int c) {
+	switch (c) {
+		case '\t':
+			printf ("\\t");
+			break;
+		case '\b':
+			printf ("\\b");
+			break;
+		case '\n':
+			printf ("\\n");
+			break;
+		case '\\':
+			printf ("\\\\");
+			break;
+		default:
+			putchar (c);
+			break;
+	}
+}
+
+static void unescape (int c) {
+	static bool in_escape = false;
+
+	if (in_escape) {
 		switch (c) {
-			case '\t':
-				printf ("\\t");
+			case 't':
+				putchar ('\t');
 				break;
-			case '\b':
-				printf ("\\b");
+			case 'b':
+				putchar ('\b');
+				break;
+			case 'n':
+				putchar ('\n');
 				break;
 			case '\\':
-				printf ("\\\\");
+				putchar ('\\');
 				break;
 			default:
 				putchar (c);
 				break;
 		}
-	}
 
-	return 0;
+		in_escape = false;
+	} else if (c == '\\')
+		in_escape = true;
+	else
+		putchar (c);
 }
