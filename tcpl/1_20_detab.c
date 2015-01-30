0e4a42c478610ab4e1ddfbf093c2cbba86e84fb6
Gwen Ives <ives.gwen@gmail.com>
2015-01-30 12:18:49 +0100

    Added tabsize parameter

--- tcpl/1_20_detab.c
+++ tcpl/1_20_detab.c
@@ -3,19 +3,42 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define TABSIZE	8
+#define DEFAULT_TABSIZE	8
 
-int main (void) {
+int main (int argc, char ** argv) {
 	int column = 0;
 	int c = 0;
+
+	int arg = 0;
+
+	int tabsize = DEFAULT_TABSIZE;
+
+	while ((arg = getopt (argc, argv, "m:")) != -1) {
+		switch (arg) {
+			int val = 0;
+
+			case 'm':
+				val = atoi (optarg);
+
+				if (val > 0)
+					tabsize = val;
+
+				break;
+			case '?':
+			default:
+				break;
+		}
+	}
 
 	while ((c = getchar ()) != EOF) {
 		if (c == '\t') {
 			do {
 				putchar (' ');
 				column++;
-			} while (column % TABSIZE != 0);
+			} while (column % tabsize != 0);
 		} else if (c == '\n') {
 			putchar (c);
 			column = 0;
--- tcpl/1_21_entab.c
+++ tcpl/1_21_entab.c
@@ -3,11 +3,34 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 
-#define TABSIZE	8
+#define DEFAULT_TABSIZE	8
 
-int main (void) {
+int main (int argc, char ** argv) {
+	int tabsize = DEFAULT_TABSIZE;
+
+	int arg = 0;
+
+	while ((arg = getopt (argc, argv, "m:")) != -1) {
+		switch (arg) {
+			int val = 0;
+
+			case 'm':
+				val = atoi (optarg);
+
+				if (val > 0)
+					tabsize = val;
+
+				break;
+			case '?':
+			default:
+				break;
+		}
+	}
+
 	bool in_spaces = false;
 
 	int column = 0;
@@ -24,16 +47,19 @@ int main (void) {
 
 			column++;
 		} else {
-			if (in_spaces) {
-				int tab_start = spaces_start / TABSIZE;
-				int tab_stop = column / TABSIZE;
+			if (in_spaces && column == spaces_start + 1) {
+				putchar (' ');
+				in_spaces = false;
+			} else if (in_spaces) {
+				int tab_start = spaces_start / tabsize;
+				int tab_stop = column / tabsize;
 
 				if (tab_start < tab_stop)
-					spaces_start = tab_start * TABSIZE;
+					spaces_start = tab_start * tabsize;
 
-				while (spaces_start + TABSIZE <= column) {
+				while (spaces_start + tabsize <= column) {
 					putchar ('\t');
-					spaces_start += TABSIZE;
+					spaces_start += tabsize;
 				}
 
 				while (spaces_start < column) {
