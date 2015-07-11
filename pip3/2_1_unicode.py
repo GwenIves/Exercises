8ad7ff27baef4f2e2966ca846f44b17c8e4d94f3
Gwen Ives <ives.gwen@gmail.com>
2015-07-11 20:05:32 +0200

    Added chapter 2 exercises

---
+++ pip3/2_1_unicode.py
@@ -0,0 +1,36 @@
+#!/bin/env python3
+
+import sys
+import unicodedata
+
+def matches_all (word, patterns):
+	word = word.lower ()
+
+	for pattern in patterns:
+		if not pattern in word:
+			return False
+
+	return True
+
+def print_table (words):
+	for i in range (len (words)):
+		words[i] = words[i].lower ()
+
+	print ("decimal   hex      chr {:^40}".format ("name"))
+	print ("------- ----- -------- {}".format ("-" * 40))
+
+	for code in range (ord (" "), sys.maxunicode + 1):
+		c = chr (code)
+
+		try:
+			name = unicodedata.name (c)
+		except ValueError:
+			continue
+
+		if not words or matches_all (name, words):
+			print ("{0:7} {0:5X} {1!r:8} {2}".format (code, str (c), name.title ()))
+
+def main ():
+	print_table (sys.argv[1:])
+
+main ()
