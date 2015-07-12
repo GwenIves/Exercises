e6d640d6afcf48c7908cad2003f1136383f38543
Gwen Ives <ives.gwen@gmail.com>
2015-07-12 15:02:58 +0200

    Added csv2html

---
+++ pip3/2_3_csv2html.py
@@ -0,0 +1,101 @@
+#!/bin/env python3
+
+import sys
+import csv
+import io
+import optparse
+import xml.sax.saxutils
+
+max_width = 100
+
+def print_start ():
+	print ("<table border='1'>")
+
+def print_end ():
+	print ("</table>")
+
+def extract_fields (line):
+	try:
+		reader = csv.reader (io.StringIO (line), delimiter = ',')
+		return list (reader)[0]
+	except IndexError:
+		return []
+
+def print_line (line, options):
+	print ("<tr bgcolor='{}'>".format (options["color"]))
+
+	fields = extract_fields (line)
+
+	maxwidth = options["maxwidth"]
+
+	for field in fields:
+		if not field:
+			print ("<td></td>")
+		else:
+			try:
+				x = float (field.replace (",", ""))
+				print ("<td align='right'>{0:{1}}</td>".format (round (x), options["format"]))
+			except ValueError:
+				field = field.title ()
+
+				if len (field) > maxwidth:
+					field = field[:maxwidth] + "..."
+
+				field = xml.sax.saxutils.escape (field)
+				print ("<td>{}</td>".format (field))
+
+	print ("</tr>")
+
+def process_options ():
+	parser = optparse.OptionParser ()
+
+	parser.add_option ("-w", "--maxwidth", help = "Maximum width of string fields (100 default)", default = 100)
+	parser.add_option ("-f", "--format", help = "Print format for numeric fields (.0f default)", default = ".0f")
+
+	(opts, args) = parser.parse_args ()
+
+	options = {}
+
+	try:
+		"{0:{1}}".format (1.0, opts.format)
+		options["format"] = opts.format
+	except ValueError:
+		options["format"] = ".0f"
+
+	try:
+		if 10 < int (opts.maxwidth) < 1000:
+			options["maxwidth"] = int (opts.maxwidth)
+		else:
+			options["maxwidth"] = 100
+	except ValueError:
+		options["maxwidth"] = 100
+
+	return options
+
+def main ():
+	opts = process_options ()
+
+	print_start ()
+
+	lineno = 0
+
+	while True:
+		try:
+			line = input ()
+
+			if lineno == 0:
+				opts["color"] = "lightgreen"
+			elif lineno % 2 == 0:
+				opts["color"] = "white"
+			else:
+				opts["color"] = "lightyellow"
+
+			print_line (line, opts)
+
+			lineno += 1
+		except EOFError:
+			break
+
+	print_end ()
+
+main ()
