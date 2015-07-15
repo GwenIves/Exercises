55ee00f4d71e8a37337bd46f305129d1841b59e5
Gwen Ives <ives.gwen@gmail.com>
2015-07-15 16:47:11 +0200

    Added external_sites

---
+++ pip3/3_1_external_sites.py
@@ -0,0 +1,44 @@
+#!/bin/env python3
+
+import sys
+import collections
+
+def main ():
+	sites = collections.defaultdict (set)
+
+	for filename in sys.argv[1:]:
+		for line in open (filename):
+			add_sites (line, sites, filename)
+
+	print_sites (sites)
+
+def add_sites (line, sites, filename):
+	i = 0
+	size = len (line)
+
+	while True:
+		site = None
+
+		i = line.find ("http://", i)
+
+		if i == -1:
+			break
+
+		i += 7
+
+		for j in range (i, size):
+			if not line[j].isalnum () and line[j] not in ".-":
+				site = line[i:j]
+				break
+
+		if site:
+			sites[site].add (filename)
+
+def print_sites (sites):
+	for site in sorted (sites):
+		print ("{} is referred to in:".format (site))
+
+		for filename in sorted (sites[site], key=str.lower):
+			print ("\t{}".format (filename))
+
+main ()
